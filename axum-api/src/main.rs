e77a013bd12a757e17ff3369f1bf67b787950f65
azujuuuuuun <35828118+azujuuuuuun@users.noreply.github.com>
2023-03-25 00:32:19 +0900

    Add find_all endpoint

--- axum-api/src/main.rs
+++ axum-api/src/main.rs
@@ -2,18 +2,16 @@ mod user;
 
 use std::net::SocketAddr;
 
-use axum::{
-    routing::{get, post},
-    Router,
-};
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/users", post(user::handler::create_user));
+    let app = Router::new().route("/", get(root)).route(
+        "/users",
+        get(user::handler::find_all).post(user::handler::create_user),
+    );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
--- axum-api/src/user/handler.rs
+++ axum-api/src/user/handler.rs
@@ -8,6 +8,21 @@ pub struct CreateUser {
     username: String,
 }
 
+pub async fn find_all() -> (StatusCode, Json<Vec<User>>) {
+    let users = vec![
+        User {
+            id: 1337,
+            username: "user1".to_string(),
+        },
+        User {
+            id: 1338,
+            username: "user2".to_string(),
+        },
+    ];
+
+    (StatusCode::OK, Json(users))
+}
+
 pub async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
     let user = User {
         id: 1337,
